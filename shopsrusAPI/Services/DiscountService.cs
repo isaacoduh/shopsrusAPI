304fa6693ecaf62d59d00bc69a2186a4a12112d8
Isaac Oduh <ufuomaoduh@gmail.com>
2021-10-24 22:44:22 +0100

    added discount service implementation

---
+++ shopsrusAPI/Services/DiscountService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using shopsrusAPI.Data;
+using shopsrusAPI.Models;
+
+namespace shopsrusAPI.Services
+{
+    public class DiscountService : IDataRepository<Discount>
+    {
+        readonly AppDbContext _context;
+        public DiscountService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Discount> GetAll()
+        {
+            return _context.Discounts.ToList();
+        }
+
+        public Discount Get(int id)
+        {
+            return _context.Discounts.FirstOrDefault(d => d.Id == id);
+        }
+
+        public void Add(Discount entity)
+        {
+            _context.Discounts.Add(entity);
+            _context.SaveChanges();
+        }
+
+        void IDataRepository<Discount>.Update(Discount dbEntity, Discount entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IDataRepository<Discount>.Delete(Discount entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
